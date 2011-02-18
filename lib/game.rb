8b97ceab99c35cf97c22093e1f24e11c1134e126
Glenn <gleenn@gmail.com>
2011-02-18 12:49:15 -0800

    WIP turn taking

--- lib/game.rb
+++ lib/game.rb
@@ -2,18 +2,44 @@ dir = File.dirname(__FILE__)
 require "#{dir}/board"
 
 class Game
+  attr_reader :first_name, :second_name, :first_board, :second_board
+
   def start
-      setup_boards
-      puts to_s
+    setup_boards
+    loop do
+      puts "#{first_name}'s Board:"
+      puts first_board
+      puts
+      puts "#{second_name}'s Board:"
+      puts second_board
+
+      break unless take_turn(first_name, second_board)
+      break unless take_turn(second_name, first_board)
+    end
   end
 
   private
 
-  def setup_boards
-    first_name, first_board = setup_board
-    second_name, second_board = setup_board
+  def take_turn(name, other_board)
+    coords = prompt_to_hit(name)
+    board.attack(coords)
+    board.ships_left?
+  end
 
-    
+  def prompt_to_hit(name)
+    puts "#{name}'s Turn"
+    puts "Enter x coord to hit: "
+    x_coord = gets.chomp
+    puts "Enter y coord to hit: "
+    y_coord = gets.chomp
+    [y_coord, x_coord]
+  end
+
+  def setup_boards
+    @first_name, @first_board = setup_board
+    @second_name, @second_board = setup_board
+
+
   end
 
   def setup_board
@@ -21,7 +47,7 @@ class Game
     name = gets.chomp
 
     begin
-      board = Board.new(10,10).generate(5)
+      board = Board.new(10, 10).generate(5)
       puts board
       puts
       puts "Do you like your board? (y/N)"
